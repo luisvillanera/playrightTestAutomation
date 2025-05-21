72b0177fe73280d0e1bcc140a080a682ba8f7b0f
luisvillanera <luis.villanera@gmail.com>
2025-05-21 14:07:01 -0300

    runtest command

--- setup_path.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Agregando el directorio actual al PATH...
-setx PATH "%PATH%;%CD%"
-echo.
-echo El directorio ha sido agregado al PATH.
-echo Por favor, reinicia tu terminal para que los cambios surtan efecto.
-echo.
-pause 
